0fe540eaf5ff81a320314b8db92095288d09681c
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-05-04 18:02:37 +0100

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+string num1;
+string num2;
+double doubleNum1;
+double doubleNum2;
+string op;
+
+Console.Write("Enter in your first number: ");
+
+num1 = Console.ReadLine();
+
+doubleNum1 = Convert.ToDouble(num1);
+
+Console.Write("Enter in your second number: ");
+
+num2 = Console.ReadLine();
+
+doubleNum2 = Convert.ToDouble(num2);
+
+Console.Write("Enter in your operator: ");
+
+op = Console.ReadLine();
+
+if (op == "+")
+{
+    Console.WriteLine(doubleNum1 + doubleNum2);
+}
+
+else if (op == "-")
+{
+    Console.WriteLine(doubleNum1 - doubleNum2);
+}
+
+else if (op == "*")
+{
+    Console.WriteLine(doubleNum1 * doubleNum2);
+}
+
+else if (op == "/")
+{
+    Console.WriteLine(doubleNum1 / doubleNum2);
+}
